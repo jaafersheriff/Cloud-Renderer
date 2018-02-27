da5e5e3650890b98c760f9932af4562311a29339
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-02-27 14:39:13 -0800

    First ImGui pane!

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,11 @@
 
 #include "Cloud.hpp"
 
-/* Initial values */
+#include "ThirdParty/imgui/imgui.h"
 
+#include <functional>
+
+/* Initial values */
 std::string RESOURCE_DIR = "../res/";
 
 /* Light position */
@@ -21,30 +24,87 @@ BillboardShader *billboardShader;
 VolumeShader *volumeShader;
 DiffuseShader *diffuseShader;
 
+/* Geometry */
 Mesh *quad;
 Mesh *cube;
 
-// TODO : imgui
-void takeInput() {
-    /* Update light */
-    if (Keyboard::isKeyPressed(GLFW_KEY_Z)) {
-        lightPos.x += 10.f;
+/* ImGui functions */
+struct ImGuiFunc { 
+    std::string name;
+    std::function<void()> fun;
+};
+std::vector<ImGuiFunc> imGuiFuncs;
+
+void initGeom();
+void createImGuiPanes();
+int main() {
+    /* Init window, keyboard, and mouse wrappers */
+    if (Window::init("Clouds")) {
+        std::cerr << "ERROR" << std::endl;
+        return 1;
     }
-    if (Keyboard::isKeyPressed(GLFW_KEY_X)) {
-        lightPos.x -= 10.f;
+
+    /* Create quad and cube */
+    initGeom();
+
+    /* Create light */
+    lightPos = glm::vec3(100.f, 100.f, 100.f);
+
+    /* Create shaders */
+    billboardShader = new BillboardShader(RESOURCE_DIR + "cloud_vert.glsl", RESOURCE_DIR + "cloud_frag.glsl");
+    billboardShader->init(RESOURCE_DIR + "cloud.png", RESOURCE_DIR + "cloudMap.png", quad);
+    volumeShader = new VolumeShader(RESOURCE_DIR + "voxelize_vert.glsl", RESOURCE_DIR + "voxelize_frag.glsl");
+    volumeShader->init(32, glm::vec2(-20.f, 20.f), glm::vec2(-20.f, 15.f), glm::vec2(-12.f, 12.f));
+    diffuseShader = new DiffuseShader(RESOURCE_DIR + "diffuse_vert.glsl", RESOURCE_DIR + "diffuse_frag.glsl");
+    diffuseShader->init();
+
+    /* Init ImGui Panes */
+    createImGuiPanes();
+
+    /* Init rendering */
+    GLSL::checkVersion();
+    CHECK_GL_CALL(glEnable(GL_DEPTH_TEST));
+    CHECK_GL_CALL(glEnable(GL_BLEND));
+    CHECK_GL_CALL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+    /* First render pass - generate volume */
+    Camera::update(Window::timeStep);
+    volumeShader->voxelize(billboardShader->quad, glm::vec3(5.f, 0.f, 0.f), glm::vec3(2.f));
+
+    while (!Window::shouldClose()) {
+        /* Update context */
+        Window::update();
+
+        /* Update camera */
+        Camera::update(Window::timeStep);
+
+        /* IMGUI */
+        if (Window::isImGuiEnabled()) {
+            for (auto func : imGuiFuncs) {
+                ImGui::Begin(func.name.c_str());
+                func.fun();
+                ImGui::End();
+            }
+        }
+
+        /* Render */
+        CHECK_GL_CALL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+        CHECK_GL_CALL(glClearColor(0.f, 0.4f, 0.7f, 1.f));
+        billboardShader->render(lightPos);
+        diffuseShader->render(cube, volumeShader->getVoxelData(), lightPos);
+        volumeShader->renderMesh(quad, glm::vec3(5.f, 0.f, 0.f), glm::vec3(2.f), false);
+        if (Window::isImGuiEnabled()) {
+            ImGui::Render();
+        }
     }
-    if (Keyboard::isKeyPressed(GLFW_KEY_C)) {
-        lightPos.y += 10.f;
-    }
-    if (Keyboard::isKeyPressed(GLFW_KEY_V)) {
-        lightPos.y -= 10.f;
-    }
-    if (Keyboard::isKeyPressed(GLFW_KEY_B)) {
-        lightPos.z += 10.f;
-    }
-    if (Keyboard::isKeyPressed(GLFW_KEY_N)) {
-        lightPos.z -= 10.f;
-    }
+}
+
+void createImGuiPanes() {
+    imGuiFuncs.push_back({ "Light",
+        [&]() {
+            ImGui::SliderFloat3("Position", glm::value_ptr(lightPos), -1000.f, 1000.f);
+        } 
+    });
 }
 
 void initGeom() {
@@ -100,51 +160,4 @@ void initGeom() {
     cube->init();
 }
 
-int main() {
-    /* Init window, keyboard, and mouse wrappers */
-    if (Window::init("Clouds")) {
-        std::cerr << "ERROR" << std::endl;
-        return 1;
-    }
 
-    /* Create quad and cube */
-    initGeom();
-
-    /* Create light */
-    lightPos = glm::vec3(100.f, 100.f, 100.f);
-
-    /* Create shaders */
-    billboardShader = new BillboardShader(RESOURCE_DIR + "cloud_vert.glsl", RESOURCE_DIR + "cloud_frag.glsl");
-    billboardShader->init(RESOURCE_DIR + "cloud.png", RESOURCE_DIR + "cloudMap.png", quad);
-    volumeShader = new VolumeShader(RESOURCE_DIR + "voxelize_vert.glsl", RESOURCE_DIR + "voxelize_frag.glsl");
-    volumeShader->init(32, glm::vec2(-20.f, 20.f), glm::vec2(-20.f, 15.f), glm::vec2(-12.f, 12.f));
-    diffuseShader = new DiffuseShader(RESOURCE_DIR + "diffuse_vert.glsl", RESOURCE_DIR + "diffuse_frag.glsl");
-    diffuseShader->init();
-
-    /* Init rendering */
-    GLSL::checkVersion();
-    CHECK_GL_CALL(glEnable(GL_DEPTH_TEST));
-    CHECK_GL_CALL(glEnable(GL_BLEND));
-    CHECK_GL_CALL(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-    /* First render pass - generate volume */
-    Camera::update(Window::timeStep);
-    volumeShader->voxelize(billboardShader->quad, glm::vec3(5.f, 0.f, 0.f), glm::vec3(2.f));
-
-    while (!Window::shouldClose()) {
-        /* Update context */
-        Window::update();
-
-        /* Update camera */
-        Camera::update(Window::timeStep);
-
-        takeInput();
-
-        /* Render */
-        CHECK_GL_CALL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
-        CHECK_GL_CALL(glClearColor(0.f, 0.4f, 0.7f, 1.f));
-        billboardShader->render(lightPos);
-        diffuseShader->render(cube, volumeShader->getVoxelData(), lightPos);
-        volumeShader->renderMesh(quad, glm::vec3(5.f, 0.f, 0.f), glm::vec3(2.f), false);
-    }
-}
