3f6d0c8d17ed2b28e98dad86f4fb4aa7c1ce14c8
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-03-22 19:16:03 -0700

    Basic light shader

---
+++ src/Shaders/LightShader.cpp
@@ -0,0 +1,46 @@
+#include "LightShader.hpp"
+
+LightShader::LightShader(const std::string vert, const std::string frag) :
+    Shader(vert, frag) {
+    lightMap = new Texture();
+}
+
+bool LightShader::init(int size) {
+    if (!Shader::init()) {
+        return false;
+    }
+
+    addAttribute("vertPos");
+
+    lightMap->width = lightMap->height = size;
+    initFBO();
+
+    return true;
+}
+
+void LightShader::render() {
+    // TODO
+}
+
+void LightShader::initFBO() {
+    // generate the FBO for the shadow depth
+    glGenFramebuffers(1, &fboHandle);
+
+    // generate the texture
+    glGenTextures(1, &lightMap->textureId);
+    glBindTexture(GL_TEXTURE_2D, lightMap->textureId);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, lightMap->width, lightMap->height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    // bind with framebuffer's depth buffer
+    glBindFramebuffer(GL_FRAMEBUFFER, fboHandle);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, lightMap->textureId, 0);
+    glDrawBuffer(GL_NONE);
+    //glReadBuffer(GL_NONE);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
---
+++ src/Shaders/LightShader.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef _LIGHT_SHADER_HPP_
+#define _LIGHT_SHADER_HPP_
+
+#include "Shader.hpp"
+#include "Model/Texture.hpp"
+
+class LightShader : public Shader {
+    public:
+        LightShader(const std::string vert, const std::string frag);
+
+        bool init(int);
+
+        void render();
+
+    private:
+        Texture * lightMap;
+        void initFBO();
+        GLuint fboHandle;
+
+        glm::mat4 L;
+};
+
+#endif
