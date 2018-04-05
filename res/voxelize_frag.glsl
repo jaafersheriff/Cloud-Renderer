8218ec9e0109136ff7e0955c4a21f0c3a4c9cbec
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-04-05 00:02:17 -0700

    Fixes and comments

--- res/voxelize_frag.glsl
+++ res/voxelize_frag.glsl
@@ -33,11 +33,20 @@ ivec3 voxelIndex(vec3 pos) {
 }
 
 void main() {
-    float dist = (distance(center, fragPos)/scale) * -1 + 1;
-    color = vec4(dist);
+    // Radius of circle contained within quad 
+    float radius = scale/2;
+
+    // Calculate the distance ratio between the center of the circle and
+    // the current fragment. This value is 1 for fragments at the center of the 
+    // circle and 0 for fragments at the edge of the circle
+    float distR = (distance(center, fragPos)/radius) * -1 + 1;
+
+    color = vec4(distR);
     if(voxelize) {
-        float radius = scale/2;
-        for(float j = 0; j < radius * dist; j += 0.2f) {
+        // Store data in the volume starting from the billboard and moving
+        // towards its normal in increments of 0.2 until reaching the edge of
+        // the sphere
+        for(float j = 0; j < radius * distR; j += 0.2f) {
             imageStore(volume, voxelIndex(fragPos + normal * j), vec4(1, 0, 0, 0));
         }
     }
