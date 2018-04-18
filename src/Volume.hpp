6c78de87322cccbcdf6ba26b3997fe506d27bcd5
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-04-18 13:38:41 -0700

    Readability

--- src/Volume.hpp
+++ src/Volume.hpp
@@ -24,16 +24,15 @@ class Volume {
 
         glm::vec3 quadPosition; // world-position of representing billboard
         glm::vec2 quadScale;    // size of representing billboard
-        glm::vec2 xBounds;  // Min and max x-mapping in world-space
-        glm::vec2 yBounds;  // Min and max y-mapping in world-space
-        glm::vec2 zBounds;  // Min and max z-mapping in world-space
-        int dimension;      // Voxels per dimension 
+        glm::vec2 xBounds;      // Min and max x-mapping in world-space
+        glm::vec2 yBounds;      // Min and max y-mapping in world-space
+        glm::vec2 zBounds;      // Min and max z-mapping in world-space
+        int dimension;          // Voxels per dimension 
 
-        int voxelCount = 0; // Count of voxels containing any data
+        int voxelCount = 0;     // Count of voxels containing any data
         glm::vec3 voxelSize;    // World-size of individual voxels
 
         int levels;         // Mipmap levels
-        int activeLevel;    // Current level
 
         GLuint volId;
 
