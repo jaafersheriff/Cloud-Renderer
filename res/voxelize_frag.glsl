25393acfa86a10225ded5ba1f06322906141a9a0
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-04-30 22:07:42 -0700

    Single cone

--- res/voxelize_frag.glsl
+++ res/voxelize_frag.glsl
@@ -53,20 +53,6 @@ ivec3 calculateVoxelIndex(vec3 pos) {
 	return ivec3(calculateVoxelLerp(pos));
 }
 
-mat4 rotationMatrix(vec3 axis, float angle)
-{
-    axis = normalize(axis);
-    float s = sin(angle);
-    float c = cos(angle);
-    float oc = 1.0 - c;
-    
-    return mat4(
-        oc*axis.x*axis.x+c,        oc*axis.x*axis.y-axis.z*s, oc*axis.z*axis.x+axis.y*s, 0.0,
-        oc*axis.x*axis.y+axis.z*s, oc*axis.y*axis.y+c,        oc*axis.y*axis.z-axis.x*s, 0.0,
-        oc*axis.z*axis.x-axis.y*s, oc*axis.y*axis.z+axis.x*s, oc*axis.z*axis.z+c,        0.0,
-        0.0,                       0.0,                       0.0,                       1.0);
-}
-
 vec3 traceCone(sampler3D voxelTexture, vec3 position, vec3 direction, int steps, float bias, float coneAngle, float coneHeight) {
     direction = normalize(direction);
     direction.z = -direction.z;
@@ -132,30 +118,9 @@ void main() {
     }
     /* Cone Trace */
     else if (voxelizeStage == 3 && sphereContrib > 0.f) {
-        /* Cone params */
-        vec3 coneDirs[4] = vec3[] (
-            vec3( 0.707, 0.707, 0),
-            vec3( 0, 0.707, 0.707),
-            vec3(-0.707, 0.707, 0),
-            vec3( 0, 0.707, -0.707)
-        );
-        float coneWeights[4] = float[](0.25, 0.25, 0.25, 0.25);
-
-        /* Rotation matrix so cones face the light source */
-        vec3 direction = normalize(lightPos - center); // TODO : upload R? fragpos? 
-        vec3 axis = cross(vec3(0, 1, 0), direction);
-        float angle = acos(dot(vec3(0, 1, 0), direction));
-        mat4 rotation = rotationMatrix(axis, angle);
-
-        /* Interpolated position */
         vec3 voxelPosition = calculateVoxelLerp(fragPos);
-
-        vec3 indirect = vec3(0);
-        for (int i = 0; i < 4; i++) {
-            /* Rotate cones to face light source */
-            vec3 dir = (vec4(coneDirs[i],1.f) * rotation).xyz;
-            indirect += coneWeights[i] * traceCone(volumeTexture, voxelPosition, dir, vctSteps, vctBias, vctConeAngle, vctConeInitialHeight);
-        }
+        vec3 dir = lightPos - fragPos; 
+        vec3 indirect = traceCone(volumeTexture, voxelPosition, dir, vctSteps, vctBias, vctConeAngle, vctConeInitialHeight);
         color = vec4(indirect, sphereContrib);
     }
 
