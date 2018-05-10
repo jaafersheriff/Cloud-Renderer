6d5eb263835f071905385664b9d2f4596c6dda4b
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-05-09 17:30:35 -0700

    Meeting

--- res/conetrace_frag.glsl
+++ res/conetrace_frag.glsl
@@ -11,10 +11,10 @@ uniform vec2 xBounds;
 uniform vec2 yBounds;
 uniform vec2 zBounds;
 uniform vec3 lightPos;
+uniform vec3 camPos;
 
 uniform sampler3D volumeTexture;
 uniform int vctSteps;
-uniform float vctBias;
 uniform float vctConeAngle;
 uniform float vctConeInitialHeight;
 uniform float vctLodOffset;
@@ -39,21 +39,29 @@ ivec3 calculateVoxelIndex(vec3 pos) {
 	return ivec3(calculateVoxelLerp(pos));
 }
 
-vec3 traceCone(sampler3D voxelTexture, vec3 position, vec3 direction, int steps, float bias, float coneAngle, float coneHeight) {
+vec3 traceCone(sampler3D voxelTexture, vec3 position, vec3 direction, int steps, float coneAngle, float coneHeight) {
     direction = normalize(direction);
-    direction.z = -direction.z;
     direction /= voxelDim;
-    vec3 start = position / voxelDim + bias * direction;
+    position /= voxelDim;
+    // position = clamp(position, 0.f, 1.f);
+
+    // vec4 c = textureLod(voxelTexture, position, 0);
+    // if (c.a == 0.f) {
+    //     return vec3(1,0,0);
+    // }
+    // else {
+    //    return c.xyz;
+    // }
 
     vec3 color = vec3(0);
-
     for (int i = 1; i <= steps; i++) {
         float coneRadius = coneHeight * tan(coneAngle / 2.f);
         float lod = log2(max(1.f, 2.f * coneRadius));
-        vec4 sampleColor = textureLod(voxelTexture, start + coneHeight * direction, lod + vctLodOffset);
-        color += sampleColor.rgb * i/steps;
+        vec4 sampleColor = textureLod(voxelTexture, position + coneHeight * direction, lod + vctLodOffset);
+        color += sampleColor.rgb * float(i)/steps; // TODO : linear scaling
         coneHeight += coneRadius;
     }
+
     return color;
 }
 
@@ -64,8 +72,10 @@ void main() {
     float sphereContrib = (distance(center, fragPos)/radius);
     sphereContrib = sqrt(max(0, 1 - sphereContrib * sphereContrib));
 
-    vec3 voxelPosition = calculateVoxelLerp(fragPos);
-    vec3 dir = lightPos - fragPos; 
-    vec3 indirect = traceCone(volumeTexture, voxelPosition, dir, vctSteps, vctBias, vctConeAngle, vctConeInitialHeight);
+    /* Start at voxel closest to camera */
+    vec3 pos = fragPos + normalize(camPos - center) * radius * sphereContrib;
+    vec3 voxelPosition = calculateVoxelLerp(pos);
+    vec3 dir = lightPos - pos; 
+    vec3 indirect = traceCone(volumeTexture, voxelPosition, dir, vctSteps, vctConeAngle, vctConeInitialHeight);
     color = vec4(indirect, sphereContrib);
 }
