1258dd73735e752258fe89e7204a6b1ac2ad29f9
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-05-16 10:50:39 -0700

    Proper order of operations

--- src/Shaders/ConeTraceShader.cpp
+++ src/Shaders/ConeTraceShader.cpp
@@ -11,43 +11,49 @@ ConeTraceShader::ConeTraceShader(std::string v, std::string f) :
 
 void ConeTraceShader::coneTrace(Cloud *cloud) {
     bind();
+
+    /* Bind cone tracing params */
+    loadInt(getUniform("vctSteps"), vctSteps);
+    loadFloat(getUniform("vctConeAngle"), vctConeAngle);
+    loadFloat(getUniform("vctConeInitialHeight"), vctConeInitialHeight);
+    loadFloat(getUniform("vctLodOffset"), vctLodOffset);
+
+    loadVector(getUniform("lightPos"), Light::spatial.position);
+    loadInt(getUniform("numBoards"), cloud->volumes.size());
+
+    /* Bind quad */
+    CHECK_GL_CALL(glBindVertexArray(Library::quad->vaoId));
+
+    /* Bind P V Vi*/
+    loadMatrix(getUniform("P"), &Camera::getP());
+    loadMatrix(getUniform("V"), &Camera::getV());
+    glm::mat4 Vi = Camera::getV();
+    Vi[3][0] = Vi[3][1] = Vi[3][2] = 0.f;
+    Vi = glm::transpose(Vi);
+    loadMatrix(getUniform("Vi"), &Vi);
+
     for (auto volume : cloud->volumes) {
         bindVolume(cloud->spatial.position, volume);
-
-        /* Bind cone tracing params */
-        loadInt(getUniform("vctSteps"), vctSteps);
-        loadFloat(getUniform("vctConeAngle"), vctConeAngle);
-        loadFloat(getUniform("vctConeInitialHeight"), vctConeInitialHeight);
-        loadFloat(getUniform("vctLodOffset"), vctLodOffset);
 
         /* Cone trace from the camera's perspective */
         loadVector(getUniform("center"), cloud->spatial.position + volume->spatial.position);
         loadFloat(getUniform("scale"), volume->spatial.scale.x);
-        loadVector(getUniform("lightPos"), Light::spatial.position);
 
-        /* Bind quad */
-        CHECK_GL_CALL(glBindVertexArray(Library::quad->vaoId));
-
-        loadMatrix(getUniform("P"), &Camera::getP());
-        loadMatrix(getUniform("V"), &Camera::getV());
-        glm::mat4 Vi = Camera::getV();
-        Vi[3][0] = Vi[3][1] = Vi[3][2] = 0.f;
-        Vi = glm::transpose(Vi);
-        loadMatrix(getUniform("Vi"), &Vi);
-
+       /* Bind M N */
         glm::mat4 M = glm::translate(glm::mat4(1.f), cloud->spatial.position + volume->spatial.position);
         M *= glm::scale(glm::mat4(1.f), glm::vec3(volume->spatial.scale.x));
         loadMatrix(getUniform("M"), &M);
-
         glm::mat3 N = glm::mat3(transpose(inverse(M * Vi)));
         loadMatrix(getUniform("N"), &N);
-        CHECK_GL_CALL(glDrawArrays(GL_TRIANGLE_STRIP, 0, 4));
 
-        /* Wrap up shader */
-        CHECK_GL_CALL(glBindVertexArray(0));
+        /* Cone trace! */
+        CHECK_GL_CALL(glDrawArrays(GL_TRIANGLE_STRIP, 0, 4));
 
         unbindVolume();
     }
+
+    /* Wrap up shader */
+    CHECK_GL_CALL(glBindVertexArray(0));
     unbind();
 }
 
