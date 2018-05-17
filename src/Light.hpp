6d8a89392ea567eef450d053c72c600f1eb3ef03
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-05-17 00:23:40 -0700

    Small fix

--- src/Light.hpp
+++ src/Light.hpp
@@ -16,7 +16,7 @@ class Light {
         static float farPlane;
 
         static void update(glm::vec3 lookAt, glm::vec2 xBounds, glm::vec2 yBounds) {
-            P = glm::ortho(xBounds.x, xBounds.y, yBounds.x, yBounds.y, zBounds.x, zBounds.y);
+            P = glm::ortho(xBounds.x, xBounds.y, yBounds.x, yBounds.y, nearPlane, farPlane);
             V = glm::lookAt(spatial.position, lookAt, glm::vec3(0, 1, 0));
         }
 };
