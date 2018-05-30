a506f3520ce62f36e2b641f01929ef5941dcc168
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-05-29 20:00:17 -0700

    No need for lambda functions for imgui

--- src/main.cpp
+++ src/main.cpp
@@ -58,8 +58,7 @@ Shader * debugShader;
 std::vector<Spatial *> cloudsBillboards;
 
 /* ImGui functions */
-std::vector<std::function<void()>> imGuiFuncs;
-void createImGuiPanes();
+void runImGuiPanes();
 
 void exitError(std::string st) {
     std::cerr << st << std::endl;
@@ -96,9 +95,6 @@ int main() {
     voxelizeShader = new VoxelizeShader(RESOURCE_DIR, "billboard_vert.glsl", "first_voxelize.glsl", "second_voxelize.glsl");
     coneShader = new ConeTraceShader(RESOURCE_DIR, "billboard_vert.glsl", "conetrace_frag.glsl");
     debugShader = new Shader(RESOURCE_DIR, "billboard_vert.glsl", "debug_frag.glsl");
-
-    /* Init ImGui Panes */
-    createImGuiPanes();
 
     /* Init rendering state */
     GLSL::checkVersion();
@@ -150,139 +146,117 @@ int main() {
 
         /* IMGUI */
         if (Window::isImGuiEnabled()) {
-            for (auto func : imGuiFuncs) {
-                func();
-            }
+            runImGuiPanes();
             ImGui::Render();
         }
     }
 }
 
-void createImGuiPanes() {
-    imGuiFuncs.push_back(
-        [&]() {
-            ImGui::Begin("Stats");
-            ImGui::Text("FPS:       %d", Window::FPS);
-            ImGui::Text("dt:        %f", Window::timeStep);
-            glm::vec3 pos = Camera::getPosition();
-            glm::vec3 look = Camera::getLookAt();
-            ImGui::Text("CamPos:    (%f, %f, %f)", pos.x, pos.y, pos.z);
-            if (ImGui::Button("Vsync")) {
-                Window::toggleVsync();
-            }
-            ImGui::End();
-        } 
-    );
-    imGuiFuncs.push_back(
-        [&]() {
-            ImGui::Begin("Light");
-            ImGui::SliderFloat3("Position", glm::value_ptr(Light::spatial.position), -1000.f, 1000.f);
-            static float mapSize = 0.2f;
-            static bool showFullMap = false;
-            static bool showSmallMap = false;
-            ImGui::Checkbox("Show full map", &showFullMap);
-            ImGui::Checkbox("Show small map", &showSmallMap);
-            if (showFullMap) {
-                CHECK_GL_CALL(glClearColor(0.2f, 0.3f, 0.5f, 1.f));
-                CHECK_GL_CALL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
-                debugShader->bind();
-                CHECK_GL_CALL(glActiveTexture(GL_TEXTURE0 + voxelizeShader->positionMap->textureId));
-                CHECK_GL_CALL(glBindTexture(GL_TEXTURE_2D, voxelizeShader->positionMap->textureId));
-                debugShader->loadInt(debugShader->getUniform("positionMap"), voxelizeShader->positionMap->textureId);
-                CHECK_GL_CALL(glBindVertexArray(Library::quad->vaoId));
-                glm::mat4 M = glm::mat4(1.f);
-                debugShader->loadMatrix(debugShader->getUniform("P"), &M);
-                debugShader->loadMatrix(debugShader->getUniform("V"), &M);
-                debugShader->loadMatrix(debugShader->getUniform("Vi"), &M);
-                debugShader->loadMatrix(debugShader->getUniform("N"), &M);
-                M *= glm::scale(glm::mat4(1.f), glm::vec3(2.f));
-                debugShader->loadMatrix(debugShader->getUniform("M"), &M);
-                CHECK_GL_CALL(glDrawArrays(GL_TRIANGLE_STRIP, 0, 4));
-                CHECK_GL_CALL(glBindVertexArray(0));
-                CHECK_GL_CALL(glActiveTexture(GL_TEXTURE0));
-                debugShader->unbind();
-            }
-            if (showSmallMap) {
-                ImGui::End();
-                ImGui::Begin("Position Map");
-                ImGui::SliderFloat("Map Size", &mapSize, 0.1f, 1.f);
-                ImGui::Image((ImTextureID)voxelizeShader->positionMap->textureId, ImVec2(voxelizeShader->positionMap->width*mapSize, voxelizeShader->positionMap->height*mapSize));
-            }
-            ImGui::End();
-        } 
-    );
-    imGuiFuncs.push_back(
-        [&]() {
-            ImGui::Begin("Billboards");
-            static glm::vec3 newPos(0.f);
-            static float scale = 1.f;
-            ImGui::SliderFloat3("Offset", glm::value_ptr(newPos), -200.f, 200.f);
-            ImGui::SliderFloat("Scale", &scale, 1.f, 200.f);
-            if (ImGui::Button("Add Billboard")) {
-                volume->addCloudBoard(Spatial(newPos, glm::vec3(scale), glm::vec3(0.f)));
-            }
-            static int currBoard = 0;
-            ImGui::SliderInt("Curr board", &currBoard, 0, volume->cloudBoards.size() - 1);
-            Spatial *s = &volume->cloudBoards[currBoard];
-            ImGui::SliderFloat3("Position", glm::value_ptr(s->position), -200.f, 200.f);
-            ImGui::SliderFloat("Cscale", &s->scale.x, 1.f, 200.f);
-            if (ImGui::Button("Delete") && volume->cloudBoards.size()) {
-                volume->cloudBoards.erase(volume->cloudBoards.begin() + currBoard);
-                currBoard = glm::max(0, currBoard - 1);
-            }
-            ImGui::End();
-        } 
-    );
-    imGuiFuncs.push_back(
-        [&]() {
-            ImGui::Begin("Cloud Volume");
-            ImGui::SliderFloat3("Position", glm::value_ptr(volume->position), -20.f, 20.f);
-            float minBounds[2] = { -100.f,  0.1f };
-            float maxBounds[2] = {  -0.1f, 100.f };
-            ImGui::SliderFloat2("XBounds", glm::value_ptr(volume->xBounds), minBounds, maxBounds);
-            ImGui::SliderFloat2("YBounds", glm::value_ptr(volume->yBounds), minBounds, maxBounds);
-            ImGui::SliderFloat2("ZBounds", glm::value_ptr(volume->zBounds), minBounds, maxBounds);
-            ImGui::Checkbox("Light Voxelize!", &lightVoxelize);
-            if (ImGui::Button("Single voxelize")) {
-                volume->clearCPU();
-                voxelizeShader->voxelize(volume);
-            }
-            if (ImGui::Button("Clear")) {
-                volume->clearGPU();
-                volume->clearCPU();
-                voxelizeShader->clearPositionMap();
-            }
-            ImGui::End();
-        }
-    );
-    imGuiFuncs.push_back(
-        [&]() {
-            ImGui::Begin("Voxels");
-            ImGui::Text("Voxels in scene : %d", volume->voxelCount);
-            ImGui::Checkbox("Light view", &lightView);
-            if (ImGui::Checkbox("Render voxels", &showVoxels)) {
-                voxelShader->disableBlack = false;
-                voxelShader->disableWhite = false;
-            }
-            ImGui::Checkbox("Disable bounds", &voxelShader->disableBounds);
-            ImGui::Checkbox("Disable black", &voxelShader->disableBlack);
-            ImGui::Checkbox("Disable white", &voxelShader->disableWhite);
-            ImGui::Checkbox("Voxel outlines", &voxelShader->useOutline);
-            ImGui::SliderFloat("Voxel alpha", &voxelShader->alpha, 0.f, 1.f);
-            ImGui::End();
-        }
-    );
-    imGuiFuncs.push_back(
-        [&]() {
-            ImGui::Begin("VXGI");
-            ImGui::SliderInt("Steps", &coneShader->vctSteps, 1, 30);
-            ImGui::SliderFloat("Angle", &coneShader->vctConeAngle, 0.f, 3.f);
-            ImGui::SliderFloat("Height", &coneShader->vctConeInitialHeight, -0.5f, 3.f);
-            ImGui::SliderFloat("LOD Offset", &coneShader->vctLodOffset, 0.f, 5.f);
-            ImGui::Checkbox("Cone trace!", &coneTrace);
-            ImGui::End();
-        }
-    );
- 
+void runImGuiPanes() {
+    ImGui::Begin("Stats");
+    ImGui::Text("FPS:       %d", Window::FPS);
+    ImGui::Text("dt:        %f", Window::timeStep);
+    glm::vec3 pos = Camera::getPosition();
+    glm::vec3 look = Camera::getLookAt();
+    ImGui::Text("CamPos:    (%f, %f, %f)", pos.x, pos.y, pos.z);
+    if (ImGui::Button("Vsync")) {
+        Window::toggleVsync();
+    }
+    ImGui::End();
+
+    ImGui::Begin("Light");
+    ImGui::SliderFloat3("Position", glm::value_ptr(Light::spatial.position), -1000.f, 1000.f);
+    static float mapSize = 0.2f;
+    static bool showFullMap = false;
+    static bool showSmallMap = false;
+    ImGui::Checkbox("Show full map", &showFullMap);
+    ImGui::Checkbox("Show small map", &showSmallMap);
+    if (showFullMap) {
+        CHECK_GL_CALL(glClearColor(0.2f, 0.3f, 0.5f, 1.f));
+        CHECK_GL_CALL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+        debugShader->bind();
+        CHECK_GL_CALL(glActiveTexture(GL_TEXTURE0 + voxelizeShader->positionMap->textureId));
+        CHECK_GL_CALL(glBindTexture(GL_TEXTURE_2D, voxelizeShader->positionMap->textureId));
+        debugShader->loadInt(debugShader->getUniform("positionMap"), voxelizeShader->positionMap->textureId);
+        CHECK_GL_CALL(glBindVertexArray(Library::quad->vaoId));
+        glm::mat4 M = glm::mat4(1.f);
+        debugShader->loadMatrix(debugShader->getUniform("P"), &M);
+        debugShader->loadMatrix(debugShader->getUniform("V"), &M);
+        debugShader->loadMatrix(debugShader->getUniform("Vi"), &M);
+        debugShader->loadMatrix(debugShader->getUniform("N"), &M);
+        M *= glm::scale(glm::mat4(1.f), glm::vec3(2.f));
+        debugShader->loadMatrix(debugShader->getUniform("M"), &M);
+        CHECK_GL_CALL(glDrawArrays(GL_TRIANGLE_STRIP, 0, 4));
+        CHECK_GL_CALL(glBindVertexArray(0));
+        CHECK_GL_CALL(glActiveTexture(GL_TEXTURE0));
+        debugShader->unbind();
+    }
+    if (showSmallMap) {
+        ImGui::End();
+        ImGui::Begin("Position Map");
+        ImGui::SliderFloat("Map Size", &mapSize, 0.1f, 1.f);
+        ImGui::Image((ImTextureID)voxelizeShader->positionMap->textureId, ImVec2(voxelizeShader->positionMap->width*mapSize, voxelizeShader->positionMap->height*mapSize));
+    }
+    ImGui::End();
+
+    ImGui::Begin("Billboards");
+    static glm::vec3 newPos(0.f);
+    static float scale = 1.f;
+    ImGui::SliderFloat3("Offset", glm::value_ptr(newPos), -200.f, 200.f);
+    ImGui::SliderFloat("Scale", &scale, 1.f, 200.f);
+    if (ImGui::Button("Add Billboard")) {
+        volume->addCloudBoard(Spatial(newPos, glm::vec3(scale), glm::vec3(0.f)));
+    }
+    static int currBoard = 0;
+    ImGui::SliderInt("Curr board", &currBoard, 0, volume->cloudBoards.size() - 1);
+    Spatial *s = &volume->cloudBoards[currBoard];
+    ImGui::SliderFloat3("Position", glm::value_ptr(s->position), -200.f, 200.f);
+    ImGui::SliderFloat("Cscale", &s->scale.x, 1.f, 200.f);
+    if (ImGui::Button("Delete") && volume->cloudBoards.size()) {
+        volume->cloudBoards.erase(volume->cloudBoards.begin() + currBoard);
+        currBoard = glm::max(0, currBoard - 1);
+    }
+    ImGui::End();
+
+    ImGui::Begin("Cloud Volume");
+    ImGui::SliderFloat3("Position", glm::value_ptr(volume->position), -20.f, 20.f);
+    float minBounds[2] = { -100.f,  0.1f };
+    float maxBounds[2] = { -0.1f, 100.f };
+    ImGui::SliderFloat2("XBounds", glm::value_ptr(volume->xBounds), minBounds, maxBounds);
+    ImGui::SliderFloat2("YBounds", glm::value_ptr(volume->yBounds), minBounds, maxBounds);
+    ImGui::SliderFloat2("ZBounds", glm::value_ptr(volume->zBounds), minBounds, maxBounds);
+    ImGui::Checkbox("Light Voxelize!", &lightVoxelize);
+    if (ImGui::Button("Single voxelize")) {
+        volume->clearCPU();
+        voxelizeShader->voxelize(volume);
+    }
+    if (ImGui::Button("Clear")) {
+        volume->clearGPU();
+        volume->clearCPU();
+        voxelizeShader->clearPositionMap();
+    }
+    ImGui::End();
+
+    ImGui::Begin("Voxels");
+    ImGui::Text("Voxels in scene : %d", volume->voxelCount);
+    ImGui::Checkbox("Light view", &lightView);
+    if (ImGui::Checkbox("Render voxels", &showVoxels)) {
+        voxelShader->disableBlack = false;
+        voxelShader->disableWhite = false;
+    }
+    ImGui::Checkbox("Disable bounds", &voxelShader->disableBounds);
+    ImGui::Checkbox("Disable black", &voxelShader->disableBlack);
+    ImGui::Checkbox("Disable white", &voxelShader->disableWhite);
+    ImGui::Checkbox("Voxel outlines", &voxelShader->useOutline);
+    ImGui::SliderFloat("Voxel alpha", &voxelShader->alpha, 0.f, 1.f);
+    ImGui::End();
+
+    ImGui::Begin("VXGI");
+    ImGui::SliderInt("Steps", &coneShader->vctSteps, 1, 30);
+    ImGui::SliderFloat("Angle", &coneShader->vctConeAngle, 0.f, 3.f);
+    ImGui::SliderFloat("Height", &coneShader->vctConeInitialHeight, -0.5f, 3.f);
+    ImGui::SliderFloat("LOD Offset", &coneShader->vctLodOffset, 0.f, 5.f);
+    ImGui::Checkbox("Cone trace!", &coneTrace);
+    ImGui::End();
 }
 
