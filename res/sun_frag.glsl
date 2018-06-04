448f177628f94ccdf5c054b4fd8ca40f0286edad
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-06-04 16:11:43 -0700

    Finish GLSL shader

--- res/sun_frag.glsl
+++ res/sun_frag.glsl
@@ -1,15 +1,25 @@
 #version 330 core
 
 in vec3 fragPos;
-in vec2 fragTex;
 
-uniform mat4 Vi;
+uniform vec3 center;
 
-uniform vec3 lightPos;
+uniform vec3 innerColor;
+uniform vec3 outerColor;
+uniform float innerRadius;
+uniform float outerRadius;
 
 out vec4 color;
 
-#define halfPI 1.57079632679
-
 void main() {
+    float dist = distance(center, fragPos);
+    /* Inner circle */
+    if (dist < innerRadius) {
+        color = vec4(innerColor, 1.0);
+    }
+    /* Outer circle */
+    else {
+        float scale = (dist - innerRadius) / (outerRadius - innerRadius);
+        color = vec4(outerColor * scale + innerColor * (1 - scale), 1 - scale);
+    }
 }
