ed3c8c303e985f760aa7531c9679654bb940f249
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-06-06 14:58:39 -0700

    Still scripting

--- papers/run.sh
+++ papers/run.sh
@@ -1,8 +1,12 @@
 #/bin/bash
 
+rm senior-project.pdf
+
+cd tex
 pdflatex main.tex
 pdflatex main.tex
 
 mv main.pdf ../senior-project.pdf
 
-wsl-open ../senior-project.pdf
+cd ..
+wsl-open senior-project.pdf
