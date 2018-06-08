c9f35f62824bf941f50b56e115c6b8606d22166f
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-06-07 19:59:47 -0700

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int Window::height = 720;
 const int I_VOLUME_BOARDS = 200;
 const glm::vec3 I_VOLUME_POSITION = glm::vec3(25.f, 0.f, 0.f);
 const glm::vec2 I_VOLUME_BOUNDS = glm::vec2(-5.f, 5.f);
-const int I_VOLUME_DIMENSION = 16;
+const int I_VOLUME_DIMENSION = 32;
 const int I_VOLUME_MIPS = 4;
 CloudVolume *volume;
 
@@ -201,7 +201,6 @@ void runImGuiPanes() {
     ImGui::End();
 
     ImGui::Begin("Billboards");
-    ImGui::Checkbox("Sort", &coneShader->doSort);
     ImGui::Checkbox("Show", &coneShader->showQuad);
     static glm::vec3 newPos(0.f);
     static float scale = 1.f;
